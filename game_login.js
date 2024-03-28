8055cd6afdceb8f1b0d9d65c0b9ed59f0e47cc2c
yourlastword69 <151046862+yourlastword69@users.noreply.github.com>
2024-03-28 19:49:44 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,11 @@
+
+function addUser() {
+  player1_name = document.getElementById("player1_name_input").value;
+  player2_name = document.getElementById("player2_name_input").value;
+
+    //Set "player1_name" using localStorage.setItem() function
+	  //Set "player2_name" using localStorage.setItem() function
+
+    window.location = "game_page.html";
+}
+
